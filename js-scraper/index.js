5b25f91b4e5f8c86c14f4c144b33b149785b7b28
Brandon Ha <idevbrandon@gmail.com>
2023-03-23 04:12:09 +0900

    failed to crawl only yield part

--- js-scraper/index.js
+++ js-scraper/index.js
@@ -1,1 +1,8 @@
-console.log("hello");
+import { getDividendYield } from "./src/getData.js";
+
+const dailyDividendYiledReport = async () => {
+  const DividendYield = await getDividendYield("AAPL");
+  console.log(DividendYield);
+};
+
+dailyDividendYiledReport();
---
+++ js-scraper/src/getData.js
@@ -0,0 +1,23 @@
+// https://www.nasdaq.com/market-activity/stocks/aapl/dividend-history
+// https://seekingalpha.com/symbol/AAPL/dividends/dividend-growth
+// https://www.digrin.com/stocks/detail/AAPL/
+
+// first, find CSS selector
+// li:nth-child(2) > span.dividend-history__summary-item__value > span
+// body > div.dialog-off-canvas-main-canvas > div > main > div.page__content > div.quote-detail__content.quote-detail__content--dividend.dividend-history-content > div.layout.layout--2-col-large > div > div.dividend-history.dividend-history--loaded > ul > li:nth-child(2) > span.dividend-history__summary-item__value
+
+import axios from "axios";
+import * as cheerio from "cheerio";
+
+export const getDividendYield = async (symbol) => {
+  return await axios
+    .get(
+      `https://www.nasdaq.com/market-activity/stocks/${symbol}/dividend-history`
+    )
+    .then((html) => {
+      const $ = cheerio.load(html.data);
+      return $(
+        "div.quote-detail__content.quote-detail__content--dividend.dividend-history-content > div.layout.layout--2-col-large > div > div.dividend-history.dividend-history--loaded > ul > li:nth-child(2)"
+      ).html();
+    });
+};
